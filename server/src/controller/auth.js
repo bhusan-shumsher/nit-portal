26fd33f8b3ec41291fac2e36dbb3c7a541d5d2fd
bhusan <bhusan@bhusans-MacBook-Pro.local>
2023-10-14 21:29:50 +0545

    brcyptsjs install

--- server/src/controller/auth.js
+++ server/src/controller/auth.js
@@ -1,5 +1,5 @@
 const {validationResult} = require('express-validator');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const Staff = require('../models/staff');
