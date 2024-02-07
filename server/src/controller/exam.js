3efc7026c84a4972f7adc9cae571e7b000872ea5
bhusan <bhusan@bhusans-MacBook-Pro.local>
2024-02-07 18:56:46 +0545

    smth

--- server/src/controller/exam.js
+++ server/src/controller/exam.js
@@ -51,7 +51,7 @@ newData.examRollNumber = data[0].examRollNumber;
 newData.totalBackDue = removeEmptyObjects(backSubjects).length * 500;
 newData.date = dateStamp;
 const baseImage = toBase64(data[0].image.data);
-newData.image = `data:${data[0].image.contentType};base64,${toBase64(data[0].image)}`;
+newData.image = `data:image/png;base64,${toBase64(data[0].image.data)}`;
 newData.logo = logoToBase64('src/template/logo.jpg');
 console.log(baseImage);
 console.log(newData.image);
