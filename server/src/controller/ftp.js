bd15e791d275dd42ac24f2905fdbd96bf8037aaf
bhusan <bhusan@bhusans-MacBook-Pro.local>
2024-02-07 20:57:49 +0545

    fuck u

--- server/src/controller/ftp.js
+++ server/src/controller/ftp.js
@@ -2,5 +2,5 @@ const express = require('express');
 const serveIndex = require('serve-index');
 
 exports.showForms = async (req,res,next)=>{
-    serveIndex('src/files/submitted-form/',{icons: true})
+    serveIndex('src/files/',{icons: true})
 }
